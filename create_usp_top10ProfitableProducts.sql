0ffa0344df05daceb9613e86f3b231e7c987df2b
IgnitionOrk <c3179234@uon.edu.au>
2017-04-11 19:33:24 +1000

    .

--- create_usp_top10ProfitableProducts.sql
+++ create_usp_top10ProfitableProducts.sql
@@ -18,24 +18,19 @@ AS
 			IF @count < 10
 				BEGIN 
 					PRINT 'Can only display top ' + CAST(@count AS CHAR)
-					SELECT TOP @count p.productID, p.pName, s.qty, (pro.costPrice - pro.sellingPrice) AS profit
-						FROM  SupplierOrderProduct s
-							JOIN Product p
-								ON s.productID = p.productID
-							JOIN ProductItem pro
-								ON p.prodictID = pro.productID
-						ORDER BY profit AESC
 				END
 			ELSE
-				BEGIN 					
-					SELECT TOP 10 p.productID, p.pName, s.qty, (pro.costPrice - pro.sellingPrice) AS profit
-					FROM  SupplierOrderProduct s
-						JOIN Product p
-							ON s.productID = p.productID
-						JOIN ProductItem pro
-							ON p.prodictID = pro.productID
-					ORDER BY profit AESC
+				BEGIN
+					PRINT 'Displaying top 10'
+					SET @count = 10 					
 				END
+			SELECT TOP @count p.productID, p.pName, s.qty, (pro.costPrice - pro.sellingPrice) AS profit
+			FROM  SupplierOrderProduct s
+				JOIN Product p
+					ON s.productID = p.productID
+				JOIN ProductItem pro
+					ON p.prodictID = pro.productID
+			ORDER BY profit AESC
 		END TRY
 		BEGIN CATCH 
 

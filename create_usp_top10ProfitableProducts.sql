05abaa6c46ce8ecedf3bd283e647592dc3d75b7b
IgnitionOrk <c3179234@uon.edu.au>
2017-04-11 17:31:10 +1000

    .

--- create_usp_top10ProfitableProducts.sql
+++ create_usp_top10ProfitableProducts.sql
@@ -16,17 +16,26 @@ AS
 			
 			-- Insufficient data has been populated in the database to show the top 10.
 			IF @count < 10
-				PRINT 'Can only display top ' + CAST(@count AS CHAR)
-
-
-
-			SELECT TOP @count p.productID, p.pName, s.qty, (pro.costPrice - pro.sellingPrice) AS profit
-			FROM  SupplierOrderProduct s
-				JOIN Product p
-					ON s.productID = p.productID
-				JOIN ProductItem pro
-					ON p.prodictID = pro.productID
-			ORDER BY profit AESC
+				BEGIN 
+					PRINT 'Can only display top ' + CAST(@count AS CHAR)
+					SELECT TOP @count p.productID, p.pName, s.qty, (pro.costPrice - pro.sellingPrice) AS profit
+						FROM  SupplierOrderProduct s
+							JOIN Product p
+								ON s.productID = p.productID
+							JOIN ProductItem pro
+								ON p.prodictID = pro.productID
+						ORDER BY profit AESC
+				END
+			ELSE
+				BEGIN 					
+					SELECT TOP 10 p.productID, p.pName, s.qty, (pro.costPrice - pro.sellingPrice) AS profit
+					FROM  SupplierOrderProduct s
+						JOIN Product p
+							ON s.productID = p.productID
+						JOIN ProductItem pro
+							ON p.prodictID = pro.productID
+					ORDER BY profit AESC
+				END
 		END TRY
 		BEGIN CATCH 
 

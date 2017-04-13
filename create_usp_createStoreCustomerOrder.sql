f49dc770065347d2f043d5de980c5abe667968e7
IgnitionOrk <c3179234@uon.edu.au>
2017-04-13 14:21:02 +1000

    Started procedure

--- create_usp_createStoreCustomerOrder.sql
+++ create_usp_createStoreCustomerOrder.sql
@@ -1,5 +1,17 @@
-CREATE PROCEDURE  usp_createStoreCustomerOrder
+
+CREATE TYPE productBarcodes_TVP AS TABLE
+(
+	barcodeID VARCHAR(10),
+	PRIMARY KEY(barcodeID)
+);
+GO
+
+CREATE PROCEDURE  usp_createStoreCustomerOrder @customerID VARCHAR(10), @barcodeList productBarcodes_TVP READONLY, @employeeID VARCHAR(10)
 AS
 
 
 GO
+
+
+DROP TYPE productBarcodes_TVP
+DROP PROCEDURE usp_createStoreCustomerOrder

9d682a25b29913fb664810b43af9475b60d662fc
jamiesy <jamieannsy@jamies-mbp.newcastle.edu.au>
2017-04-27 10:23:04 +1000

    .

--- test_usp_createPayroll.sql
+++ test_usp_createPayroll.sql
@@ -1,11 +1,75 @@
-DROP TYPE payroll_TVP
-
-/*CREATE PROCEDURE usp_createPayroll
-  @startDate DATE, @endDate DATE, @
-
-  
-  
+/*
+CREATE TABLE Allowance 
+(
+	allowanceID VARCHAR(10),
+	payslipID VARCHAR(10) NOT NULL,
+	allowanceTypeID VARCHAR(10) NOT NULL,
+	amount FLOAT,
+	allowDescription VARCHAR(255),
+	FOREIGN KEY(payslipID) REFERENCES Payslip(payslipID) ON DELETE CASCADE,
+	FOREIGN KEY(allowanceTypeID) REFERENCES AllowanceType(allowanceTypeID) ON DELETE NO ACTION,
+	PRIMARY KEY(allowanceID)
 );
+GO 
+*/
+
+CREATE PROCEDURE usp_createPayroll 
+AS
+	DECLARE @payslipID VARCHAR(10);
+	DECLARE @employeeID VARCHAR(10);
+	DECLARE @taxBracketID VARCHAR(10);
+	DECLARE @startDate DATE;
+	DECLARE @endDate DATE;
+	DECLARE @workedHours FLOAT;
+	DECLARE @basePay 
+    ,@allowanceBonus AllowanceInfo READONLY
+AS
+BEGIN
+    INSERT INTO Payslip
+    SELECT @startDate
+        ,@endDate
+        ,n.hoursWorked
+        ,p.hourlyRate
+        ,p.hourlyRate * n.hoursWorked
+        ,(p.hourlyRate * n.hoursWorked) + a.allowanceAmount
+        ,((p.hourlyRate * n.hoursWorked) + a.allowanceAmount) * t.taxRate / 100
+        ,@taxID
+        ,a.allowanceID
+        ,n.employeeID
+    FROM @workedHours n
+        ,@allowanceBonus a
+        ,Position p
+        ,Employee e
+        ,Tax t
+    WHERE p.positionID = e.positionID
+        AND e.employeeID = n.employeeID
+        AND t.taxID = @taxID
+END
+
+DECLARE @employeeInfo EmployeeInfo;
+DECLARE @hoursWorked INT;
+
+INSERT @employeeInfo
+SELECT e.employeeID
+    ,@hoursWorked
+FROM Employee e
+WHERE e.employeeID = 1
+    AND @hoursWorked = 160
+
+DECLARE @allowanceInfo AllowanceInfo;
+DECLARE @empInfo EmployeeInfo
+
+INSERT @allowanceInfo
+SELECT e.employeeID
+    ,a.allowanceID
+    ,a.allowanceAmount
+FROM Employee e
+    ,Allowance a
+    ,@empInfo emp
+WHERE e.employeeID = emp.employeeID
+    AND a.allowanceID = 1
+
+EXECUTE usp_createPayroll;
 GO
 /*
 start date (input) – Start date for start of pay period
@@ -22,14 +86,3 @@ to be calculated and stored for each payslip.
 Note that all errors must be caught and handled. Appropriate error messages must be
 raised. The stored procedure must be extensively tested in the test script.
 */
-CREATE PROCEDURE usp_createPayroll
-
-AS
-
-  BEGIN
-  
-    INSERT INTO Payslip
-    SELECT @startDate, @endDate, 
-
-    
-GO

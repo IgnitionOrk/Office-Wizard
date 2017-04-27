cf5260f17c3c22c94923aba34f65a421bd8ac78d
IgnitionOrk <c3179234@uon.edu.au>
2017-04-27 12:37:50 +1000

    .

--- create_usp_top10ProfitableProducts.sql
+++ create_usp_top10ProfitableProducts.sql
@@ -9,32 +9,23 @@ AS
 	DECLARE @productName varchar(50);
 	DECLARE @qtySold int;
 	DECLARE @totalProfit float;
-
 	DECLARE pItems CURSOR FOR
-		SELECT TOP 10 ProductItem.productID, Product.pName, COUNT(*), SUM(ProductItem.sellingPrice - ProductItem.costPrice) AS profit
-		FROM ProductItem, Product
-		WHERE ProductItem.productID = Product.productID AND productItem.status = 'sold'
-		GROUP BY ProductItem.productID, Product.pName
-		ORDER BY profit DESC;
-BEGIN
-	OPEN pItems
-	FETCH NEXT FROM pItems INTO @pID, @productName, @qtySold, @totalProfit
-
-	WHILE @@FETCH_STATUS = 0
+	SELECT TOP 10 ProductItem.productID, Product.pName, COUNT(*), SUM(ProductItem.sellingPrice - ProductItem.costPrice) AS profit
+	FROM ProductItem, Product
+	WHERE ProductItem.productID = Product.productID AND productItem.status = 'sold'
+	GROUP BY ProductItem.productID, Product.pName
+	ORDER BY profit DESC;
 	BEGIN
-		PRINT (@pID + ' ' + @productName + ' ' + CAST(@qtySold AS CHAR) + ' Total profit: ' + CAST(@totalProfit AS CHAR));
-
+		OPEN pItems
 		FETCH NEXT FROM pItems INTO @pID, @productName, @qtySold, @totalProfit
+
+		WHILE @@FETCH_STATUS = 0
+		BEGIN
+			PRINT (@pID + ' ' + @productName + ' ' + CAST(@qtySold AS CHAR) + ' Total profit: ' + CAST(@totalProfit AS CHAR));
+
+			FETCH NEXT FROM pItems INTO @pID, @productName, @qtySold, @totalProfit
 	END
-
 	CLOSE pItems   
-	DEALLOCATE pItems
-	
+	DEALLOCATE pItems	
 END;
-
-GO
-
-EXECUTE usp_top10ProfitableProducts;
-GO
-DROP PROCEDURE usp_top10ProfitableProducts
 GO
--- test_usp_top10ProfitableProducts.sql
+++ test_usp_top10ProfitableProducts.sql
@@ -4,4 +4,7 @@
 --Date Created  27-Apr-2017
 --Date Modified 18-Apr-2017
 
-EXECUTE 
+EXECUTE dbo.usp_Top10ProfitableProducts
+GO
+DROP PROCEDURE dbo.usp_top10ProfitableProducts
+GO

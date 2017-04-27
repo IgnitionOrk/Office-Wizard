33826d2f49bf104a30e9cf4a2299162bac35e6ab
jamiesy <jamieannsy@jamies-mbp.newcastle.edu.au>
2017-04-27 12:39:04 +1000

    payroll so far

--- create_usp_createPayroll.sql
+++ create_usp_createPayroll.sql
@@ -1,35 +1,4 @@
-/*
-CREATE TABLE Allowance 
-(
-	allowanceID VARCHAR(10),
-	payslipID VARCHAR(10) NOT NULL,
-	allowanceTypeID VARCHAR(10) NOT NULL,
-	amount FLOAT,
-	allowDescription VARCHAR(255),
-	FOREIGN KEY(payslipID) REFERENCES Payslip(payslipID) ON DELETE CASCADE,
-	FOREIGN KEY(allowanceTypeID) REFERENCES AllowanceType(allowanceTypeID) ON DELETE NO ACTION,
-	PRIMARY KEY(allowanceID)
-);
-GO 
-*/
-
-CREATE PROCEDURE usp_createPayroll 
-AS
-	@payslipID VARCHAR(10);
-	@employeeID VARCHAR(10);
-	@taxBracketID VARCHAR(10);
-	@startDate DATE;
-	@endDate DATE;
-	@workedHours FLOAT;
-	-- @basePay ;
-	@allowanceBonus AllowanceInfo READONLY
-AS
-BEGIN
-
-
-EXECUTE usp_createPayroll;
-GO
-/*
+/*	REQUIREMENTS
 start date (input) – Start date for start of pay period
 end date (input) – End date for pay period
 employee hours worked information (input) – A table-valued parameter with employee
@@ -44,3 +13,90 @@ to be calculated and stored for each payslip.
 Note that all errors must be caught and handled. Appropriate error messages must be
 raised. The stored procedure must be extensively tested in the test script.
 */
+
+/*
+	-- auto-generate payslipID
+	CREATE PROCEDURE create_payslipID
+	(
+	   @payslipNumberID int,
+	   @payDate datetime  
+	) AS Begin 
+
+	-- calc 5th digit:
+	DECLARE @fifthDigit int;
+	SELECT 
+	    @fifthDigit = count(*) + 1
+	FROM payslipNumber AS bb
+	INNER JOIN ItemMfg ii ON ii.payslipNumberID = bb.payslipNumberID 
+	where bb.payslipNumberID = @payslipNumberID             -- single payslipNumber-row to get ItemType
+	    AND ii.payDate <= @payate                              -- all previous datetimes
+	    AND cast(ii.payDate as date) = cast(@payDate as date)   -- same day
+
+	-- ManufactureID is Identity (i.e. autoincremented)
+	INSERT INTO ItemMfg (payslipNumberID, payDate, SerialNumber)
+	    SELECT @payslipNumberID
+	        , @payDate
+	        , 'PS' + bb.ItemType + cast(@fifthDigit as varchar(5))
+	    FROM payslipNumber bb
+	    WHERE bb.payslipNumber = @payslipNumber
+	;
+
+	END
+
+*/
+
+CREATE TYPE EmployeeInfo AS TABLE 
+(
+    employeeID INT,
+    workedHours INT,
+    taxBracketID FLOAT,
+    PRIMARY KEY (
+        employeeID,
+        workedHours
+        )
+);
+GO
+
+CREATE TYPE AllowanceInfo AS TABLE 
+(
+    employeeID INT,
+    allowanceID INT,
+    allowanceAmount DECIMAL(7, 2),
+    PRIMARY KEY (
+        employeeID,
+        allowanceID,
+        allowanceAmount
+        )
+);
+GO
+
+CREATE PROCEDURE usp_createPayroll
+	@startDate	DATE,
+	@endDate	DATE,
+	@workedHours	FLOAT,
+	@noHoursWorked EmployeeInfo READONLY,
+	@allowanceBonus AllowanceInfo READONLY
+    
+AS
+BEGIN
+	INSERT INTO payslip
+		SELECT 
+		n.employeeID,
+		n.taxBracketID,
+		@startDate,
+		@endDate,
+		n.workedHours,
+		p.
+END
+
+
+
+-- Payslip (payslipID, employeeID, taxBracketID, startDate, endDate,
+--workedHours, basePay, taxPayable, netPay)
+
+
+
+
+
+
+

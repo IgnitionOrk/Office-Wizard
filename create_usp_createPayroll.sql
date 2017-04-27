964d400b96011abe2d02b98e53c10e5dce8ae726
jamiesy <jamieannsy@jamies-mbp.newcastle.edu.au>
2017-04-27 11:07:34 +1000

    .

--- create_usp_createPayroll.sql
+++ create_usp_createPayroll.sql
@@ -1,5 +1,34 @@
-CREATE PROCEDURE usp_createPayroll
+/*
+CREATE TABLE Allowance 
+(
+	allowanceID VARCHAR(10),
+	payslipID VARCHAR(10) NOT NULL,
+	allowanceTypeID VARCHAR(10) NOT NULL,
+	amount FLOAT,
+	allowDescription VARCHAR(255),
+	FOREIGN KEY(payslipID) REFERENCES Payslip(payslipID) ON DELETE CASCADE,
+	FOREIGN KEY(allowanceTypeID) REFERENCES AllowanceType(allowanceTypeID) ON DELETE NO ACTION,
+	PRIMARY KEY(allowanceID)
+);
+GO 
+*/
+
+CREATE PROCEDURE usp_createPayroll 
 AS
+	@payslipID VARCHAR(10);
+	@employeeID VARCHAR(10);
+	@taxBracketID VARCHAR(10);
+	@startDate DATE;
+	@endDate DATE;
+	@workedHours FLOAT;
+	@basePay 
+    ,@allowanceBonus AllowanceInfo READONLY
+AS
+BEGIN
+
+
+EXECUTE usp_createPayroll;
+GO
 /*
 start date (input) – Start date for start of pay period
 end date (input) – End date for pay period
@@ -15,5 +44,3 @@ to be calculated and stored for each payslip.
 Note that all errors must be caught and handled. Appropriate error messages must be
 raised. The stored procedure must be extensively tested in the test script.
 */
-
-GO

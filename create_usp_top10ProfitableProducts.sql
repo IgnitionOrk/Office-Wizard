5f2edf55734582fc1342ef36c8e96d0233468a28
micahconway <micah.conway1@gmail.com>
2017-05-01 09:57:42 +1000

    Update create_usp_top10ProfitableProducts.sql

--- create_usp_top10ProfitableProducts.sql
+++ create_usp_top10ProfitableProducts.sql
@@ -1,31 +1,36 @@
 -- Created by: Micah Conway
 -- Student number: 3232648
 -- Date created: 19-Apr-2017
--- Date modified: 20-Apr-2017
+-- Date modified: 1-May-2017
+-- Gets the 10 products, less if there are less than 10, that have made the most profit
 
 CREATE PROCEDURE usp_top10ProfitableProducts
 AS
 	DECLARE @pID varchar(10);
-	DECLARE @productName varchar(50);
+	DECLARE @productName char(40);
 	DECLARE @qtySold int;
 	DECLARE @totalProfit float;
+
+	--Cursor so that we can iterate through the data to be selected
 	DECLARE pItems CURSOR FOR
 	SELECT TOP 10 ProductItem.productID, Product.pName, COUNT(*), SUM(ProductItem.sellingPrice - ProductItem.costPrice) AS profit
 	FROM ProductItem, Product
-	WHERE ProductItem.productID = Product.productID AND productItem.status = 'sold'
+	WHERE ProductItem.productID = Product.productID AND productItem.status = 'sold'	-- we only want the products that were sold, as profit can only be made on sold items
 	GROUP BY ProductItem.productID, Product.pName
 	ORDER BY profit DESC;
+	
 	BEGIN
+		PRINT ('Product ID		Product Name							Quantity Sold					Total Profit'); --column headers
 		OPEN pItems
 		FETCH NEXT FROM pItems INTO @pID, @productName, @qtySold, @totalProfit
 
-		WHILE @@FETCH_STATUS = 0
+		WHILE @@FETCH_STATUS = 0	--while there are more lines to read from the cursor
 		BEGIN
-			PRINT (@pID + ' ' + @productName + ' ' + CAST(@qtySold AS CHAR) + ' Total profit: ' + CAST(@totalProfit AS CHAR));
+			PRINT (@pID + '			' + @productName + '	' + CAST(@qtySold AS CHAR) + '	' + CAST(@totalProfit AS CHAR));	--print the data we need
 
-			FETCH NEXT FROM pItems INTO @pID, @productName, @qtySold, @totalProfit
+			FETCH NEXT FROM pItems INTO @pID, @productName, @qtySold, @totalProfit	--loop
 		END
 	CLOSE pItems   
-	DEALLOCATE pItems	
+	DEALLOCATE pItems	--close and remove cursor
 	END;
 GO

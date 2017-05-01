37285f7a4db285450f6d559427673403be5b3ebb
micahconway <micah.conway1@gmail.com>
2017-05-01 12:00:35 +1000

    Update test_usp_createStoreCustomerOrder.sql

--- test_usp_createStoreCustomerOrder.sql
+++ test_usp_createStoreCustomerOrder.sql
@@ -1,23 +1,46 @@
+-- Created by: Ryan Cunneen, Micah Conway
+-- Student number: 3179234, 3232648
+-- Date created: 19-Apr-2017
+-- Date modified: 1-May-2017
 
-
-
-DECLARE @customer1ID VARCHAR(10)
-DECLARE @employeeID VARCHAR(10)
+--Output parameter
 DECLARE @salesOrdID VARCHAR(10)
-SET @customer1ID = 'CO0001077'
-SET @employeeID = 'E12345'
-SET @salesOrdID = '7121235337'
-DECLARE @customer1Products AS dbo.productBarcodes_TVP
 
-INSERT INTO @customer1Products VALUES('PI10000019');
-INSERT INTO @customer1Products VALUES('PI10000015');
-INSERT INTO @customer1Products VALUES('PI10000018');
---INSERT INTO @customer1Products VALUES('PI10000021');
+--test case 0
+DECLARE @customerProducts AS dbo.productBarcodes_TVP
+INSERT INTO @customerProducts VALUES('PI10000050');
+INSERT INTO @customerProducts VALUES('PI10000051');
+INSERT INTO @customerProducts VALUES('PI10000052');
+EXECUTE usp_createStoreCustomerOrder 'C2001', @customerProducts, 'E12345', @salesOrdID;
+
+--test case 1
+DECLARE @customerProducts1 AS dbo.productBarcodes_TVP
+INSERT INTO @customerProducts1 VALUES('PI10000050');
+INSERT INTO @customerProducts1 VALUES('PI10000051');
+INSERT INTO @customerProducts1 VALUES('PI10000052');
+EXECUTE usp_createStoreCustomerOrder NULL, @customerProducts1, 'E12345', @salesOrdID;
+
+--test case 2
+DECLARE @customerProducts2 AS dbo.productBarcodes_TVP
+INSERT INTO @customerProducts2 VALUES('PI10000050');
+INSERT INTO @customerProducts2 VALUES('PI10000051');
+INSERT INTO @customerProducts2 VALUES('PI10000052');
+EXECUTE usp_createStoreCustomerOrder NULL, @customerProducts2, 'E12345', @salesOrdID;
+
+--test case 3
+DECLARE @customerProducts3 AS dbo.productBarcodes_TVP
+INSERT INTO @customerProducts3 VALUES('PI10000050');
+INSERT INTO @customerProducts3 VALUES('PI10000051');
+INSERT INTO @customerProducts3 VALUES('PI10000052');
+EXECUTE usp_createStoreCustomerOrder NULL, @customerProducts3, 'E12345', @salesOrdID;
+
+--test case 4
+DECLARE @customerProducts4 AS dbo.productBarcodes_TVP
+INSERT INTO @customerProducts4 VALUES('PI10000050');
+INSERT INTO @customerProducts4 VALUES('PI10000051');
+INSERT INTO @customerProducts4 VALUES('PI10000052');
+EXECUTE usp_createStoreCustomerOrder NULL, @customerProducts4, 'E12345', @salesOrdID;
 
 
-EXECUTE usp_createStoreCustomerOrder @customer1ID, @customer1Products, @employeeID, @salesOrdID OUT
 
 
-SELECT * FROM CustOrdProduct WHERE custOrdID =  '711235337'
-
-DROP PROCEDURE usp_createStoreCustomerOrder
